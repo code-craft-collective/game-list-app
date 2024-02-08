e74536bcb5182d659b3be092c8c0f3d4e4754d30
Juan Yuste Del Valle <kurobaa@gmail.com>
2024-02-08 16:47:45 +0100

    game card

---
+++ src/copmponents/Game-Card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+
+export default function GameCard((props)=>{
+
+    const {name, genre, rating, platform} = props;
+
+
+});
+
+
