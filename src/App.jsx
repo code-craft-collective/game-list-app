9560ed332debc2aa503b3a2528921276165614cd
Frederico Rose <fredericoa.rose@gmail.com>
2024-02-08 19:07:02 +0100

    Basic Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div>Hello World</div>;
-
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
+}
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="border border-red">
+      <footer className="absolute bottom-0 text-2xl font-bold rounded-s-3xl border-8 rounded-3xl w-full bg-slate-900">
+        <p>GitHub</p>
+        <p>B</p>
+        <p>C</p>
+        <img></img>
+      </footer>
+    </div>
+  );
+};
+export default Footer;
