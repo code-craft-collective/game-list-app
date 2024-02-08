7862343f569d74b628ba2caa1565a9fd4297b23f
Juan Yuste Del Valle <kurobaa@gmail.com>
2024-02-08 17:05:43 +0100

    second attempt

--- src/copmponents/Game-Card.jsx
+++ src/copmponents/Game-Card.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
+export default function GameCard(props) {
+  const { id, name, image, genre, rating, platform } = props;
 
-export default function GameCard((props)=>{
-
-    const {name, genre, rating, platform} = props;
-
-
-});
-
-
+  return (
+    <Link to={"#"} key={id}>
+      <div className="game-card">
+        <h3>{name}</h3>
+        <h3>{name}</h3>
+        <img src={image} className="card-image" alt="thumbnail" />
+        <label className="genre-label">{genre}</label>
+        <p>{rating}</p>
+        <label className="platforms-label">{platform}</label>
+      </div>
+    </Link>
+  );
+}
