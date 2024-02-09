2caa0bdbd2b3cc878ea3375738aff7f926770096
Juan Yuste Del Valle <kurobaa@gmail.com>
2024-02-09 16:39:51 +0100

    game card + landing page, routes & routes data, api key on env

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
+import axios from "axios";
+import GamesList from "./pages/GamesList";
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div>Hello World</div>;
-
+  return (
+    <div>
+      <GamesList />
+      Hello World
+    </div>
+  );
+}
 export default App;
