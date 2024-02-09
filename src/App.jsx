7edb1c21bba0b149d011062fc4d79db65f54feb8
Emmanouil Popas <noah.popas@hotmail.com>
2024-02-09 18:39:58 +0100

    Detail page of the games - still working on it

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Navbar from "./components/Navbar";
 import "./App.css";
 import Footer from "./components/Footer";
+import ProductDetailPage from "./pages/ProductDetailPage";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Navbar />
+
       <Footer />
+      <ProductDetailPage />
     </div>
   );
 }
