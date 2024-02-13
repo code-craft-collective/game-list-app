59193ea28805e59b8c90758b31e5d34293321bf3
Juan Yuste Del Valle <kurobaa@gmail.com>
2024-02-13 16:47:06 +0100

    form created, need to implement route for separate form page

--- src/components/Game-Card.jsx
+++ src/components/Game-Card.jsx
@@ -1,13 +1,31 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
-
+import EditForm from "./Edit-Form";
 export default function GameCard(props) {
   const { id, name, image, rating, genre, platforms } = props;
+  const [showForm, setShowForm] = useState(false);
+  const [showButton, setShowButton] = useState(false);
+
+  const [savedGames, setSavedGames] = useState([]);
+  const dbURL = `https://game-app-backend.adaptable.app/saved-games`;
+
+  useEffect(() => {
+    axios
+      .get(dbURL)
+      .then((resp) => {
+        setSavedGames(resp.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const toggleForm = () => {
+    setShowForm(!showForm);
+  };
 
   return (
-    //ADD LINK to game details page
-
-    <Link to={`/games/${id}`}>
-      <div className="game-card p-10 m-10 border border-solid border-black">
+    <div className="game-card p-10 m-10 border border-solid border-black">
+      <Link to={`/games/${id}`}>
         <h3 className="text-xl my-2">{name}</h3>
         <div className="w-64">
           <img src={image} className="my-3 w-auto" alt="thumbnail" />
@@ -23,7 +41,7 @@ export default function GameCard(props) {
             </p>
           ))}
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 }
