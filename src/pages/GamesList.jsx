e6ce16c1f93bae905b76a4e66f4ce0dcd89b6b40
Juan Yuste Del Valle <kurobaa@gmail.com>
2024-02-13 20:03:16 +0100

    logic for displaying faves implemented

--- src/pages/GamesList.jsx
+++ src/pages/GamesList.jsx
@@ -2,10 +2,10 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import GameCard from "../components/Game-Card";
 
-export default function GamesList() {
+export default function GamesList({ showFavorites }) {
   const [list, setList] = useState([]);
   const [favoritesList, setFavoritesList] = useState([]);
-  const [isFavorites, setIsFavorites] = useState(false);
+  const [isFavorites, setIsFavorites] = useState(showFavorites);
   const url = `https://api.rawg.io/api/games?token&key=${
     import.meta.env.VITE_GAMES_API_KEY
   }`;
@@ -21,51 +21,52 @@ export default function GamesList() {
   }, []);
 
   useEffect(() => {
-    axios
-      .get(dbURL)
-      .then((resp) => {
-        console.log(resp.data);
-        setFavoritesList(resp.data.results);
-      })
-      .catch((err) => console.log(err));
-  }, []);
+    if (isFavorites) {
+      axios
+        .get(dbURL)
+        .then((resp) => {
+          console.log(resp.data);
+          setFavoritesList(resp.data.results);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [isFavorites]);
 
   return (
     <div className="flex flex-wrap justify-evenly h-screen overflow-auto">
-      {list.map((result) => (
-        <GameCard
-          key={result.id}
-          id={result.id}
-          name={result.name}
-          image={result.background_image}
-          rating={result.metacritic}
-          platforms={result.platforms}
-          genre={result.genres.map((e, i) => {
-            if (i === result.genres.length - 1) {
-              return e.name;
-            }
-            return e.name + ", ";
-          })}
-        />
-      ))}
-
-      {favoritesList &&
-        favoritesList.map((result) => (
-          <GameCard
-            key={result.id}
-            id={result.id}
-            name={result.name}
-            image={result.background_image}
-            rating={result.metacritic}
-            platforms={result.platforms}
-            genre={result.genres.map((e, i) => {
-              if (i === result.genres.length - 1) {
-                return e.name;
-              }
-              return e.name + ", ";
-            })}
-          />
-        ))}
+      {isFavorites
+        ? favoritesList.map((result) => (
+            <GameCard
+              key={result.id}
+              id={result.id}
+              name={result.name}
+              image={result.background_image}
+              rating={result.metacritic}
+              platforms={result.platforms}
+              genre={result.genres.map((e, i) => {
+                if (i === result.genres.length - 1) {
+                  return e.name;
+                }
+                return e.name + ", ";
+              })}
+            />
+          ))
+        : list.map((result) => (
+            <GameCard
+              key={result.id}
+              id={result.id}
+              name={result.name}
+              image={result.background_image}
+              rating={result.metacritic}
+              platforms={result.platforms}
+              genre={result.genres.map((e, i) => {
+                if (i === result.genres.length - 1) {
+                  return e.name;
+                }
+                return e.name + ", ";
+              })}
+            />
+          ))}
     </div>
   );
 }
