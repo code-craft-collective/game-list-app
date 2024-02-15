b016c53f86e92124f51fd6b528abf36302295f5b
Frederico Rose <fredericoa.rose@gmail.com>
2024-02-15 15:27:31 +0100

    Navbar_Improvements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,13 @@ import { useTheme } from "./ThemeContext";
 
 const Navbar = () => {
   const { darkMode, toggleDarkMode } = useTheme();
+
   return (
     <nav className="bg-gray-800 text-white p-4 fixed top-0 w-full z-10">
       <div className="container mx-auto flex items-center justify-between">
-        <Link to="/" className="Logo home">
-          <img src="/src/assets/Logo.png" alt="Logo" className="h-10" />
-        </Link>
-        <div className="flex items-center space-x-4">
+        <div className="flex items-center space-x-4 ml-6">
+          {" "}
+          {/* Increased ml-6 for more left margin */}
           <Link to="/" className="hover:text-gray-300">
             Home
           </Link>
@@ -18,8 +18,10 @@ const Navbar = () => {
             About
           </Link>
         </div>
-        <div>
-          {" "}
+        <Link to="/" className="Logo home">
+          <img src="/src/assets/Logo.png" alt="Logo" className="h-10" />
+        </Link>
+        <div className="flex items-center space-x-4">
           <button
             onClick={toggleDarkMode}
             className={`px-4 py-2 rounded-full ${
@@ -30,8 +32,6 @@ const Navbar = () => {
           >
             {darkMode ? "☀︎" : "☾"}
           </button>
-        </div>
-        <div>
           <Link to="/profile">
             <img
               src={ProfPic}
