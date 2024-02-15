70995994a25e9e87c40d892577b7619b18ea1aa7
Frederico Rose <fredericoa.rose@gmail.com>
2024-02-15 12:05:51 +0100

    Toast Features

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-const Toast = ({ color = "red" }) => {
+const Toast = ({ type }) => {
   const [visible, setVisible] = useState(true);
 
   const handleClose = () => {
@@ -15,17 +15,39 @@ const Toast = ({ color = "red" }) => {
     return () => clearTimeout(timeoutId);
   }, []);
 
+  let toastMessage, toastColor;
+
+  switch (type) {
+    case "success":
+      toastMessage =
+        "You have successfully added the game to your profile page";
+      toastColor = "green";
+      break;
+    case "remove":
+      toastMessage =
+        "You have successfully removed the game from your profile page";
+      toastColor = "red";
+      break;
+    default:
+      return null;
+  }
+
   const toastStyle = {
-    backgroundColor: color,
+    backgroundColor: toastColor,
     color: "white",
     padding: "1rem",
     borderRadius: "0.5rem",
     boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
     opacity: 1,
+    position: "fixed",
+    top: "1rem",
+    left: "50%",
+    transform: "translateX(-50%)",
+    zIndex: 1000,
   };
 
   return (
-    <div className={`fixed bottom-10 right-10 ${visible ? "block" : "hidden"}`}>
+    <div className={`${visible ? "block" : "hidden"}`}>
       <div style={toastStyle}>
         <div className="flex items-center">
           <div className="w-5 h-5 bg-white mr-2 rounded-full"></div>
@@ -38,7 +60,7 @@ const Toast = ({ color = "red" }) => {
             Close
           </button>
         </div>
-        <div>Hello, world! This is a toast message.</div>
+        <div>{toastMessage}</div>
       </div>
     </div>
   );
